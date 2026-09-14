b225ad5b946eea57360508bf1f739010aac5a743
Rudraksh Baranwal <rudrakshbaranwal2804@gmail.com>
2026-09-14 21:00:38 +0530

    added more details

--- 01_setup.mongodb.js
+++ 01_setup.mongodb.js
@@ -28,3 +28,36 @@ db.products.insertMany([
     createdAt: new Date(),
   },
 ]);
+
+db.contacts.insertMany([
+{ name: "Alice", message: "Loved your website!", phone: "9876543210", createdAt:
+new Date() },
+{ name: "Bob", message: "Do you have discounts on laptops?", phone: "9123456789",
+createdAt: new Date() },
+{ name: "Carol", message: "I want to cancel my order.", phone: "9988776655",
+createdAt: new Date() }
+])
+
+db.orders.insertMany([
+{
+ orderId: "ORD001",
+ user: "John Doe",
+ products: [
+{ name: "Wireless Mouse", quantity: 1, price: 799 },
+{ name: "Mechanical Keyboard", quantity: 1, price: 2499 }
+],
+ total: 3298,
+ status: "Delivered",
+ createdAt: new Date()
+},
+{
+ orderId: "ORD002",
+ user: "Jane Smith",
+ products: [
+{ name: "Gaming Laptop", quantity: 1, price: 85999 }
+],
+ total: 85999,
+ status: "Pending",
+ createdAt: new Date()
+}
+])
